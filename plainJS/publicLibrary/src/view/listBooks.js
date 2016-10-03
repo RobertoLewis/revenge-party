02ca4a257fc2aef2c0e89dc8faeedc5f4c72eb71
Roberto Lewis <roberto.lewis@gmail.com>
2016-10-03 12:55:10 -0700

    [fix] add -1 as argument to insertRow call

--- plainJS/publicLibrary/src/view/listBooks.js
+++ plainJS/publicLibrary/src/view/listBooks.js
@@ -8,7 +8,7 @@ pl.view.listBooks = {
     / for each book, create a table row with cells for the 3 attributes
     for (i = 0; i < keys.length; i++) {
       key = keys[i];
-      row = tableBodyEl.insertRow();
+      row = tableBodyEl.insertRow(-1);
       row.insertCell(-1).textContent = Book.instances[key].isbn;
       row.insertCell(-1).textContent = Book.instances[key].title;
       row.insertCell(-1).textContent = Book.instances[key].year;
